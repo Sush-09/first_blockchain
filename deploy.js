1b2c5a2c01a0c2c3c0f22c248a600e57dd7515ca
SrushtiBiwalkar <2019srushti.biwalkar@ves.ac.in>
2023-01-14 01:18:56 +0530

    new file:   deploy.js 	modified:   package.json

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'moment census surge remind pupil keep genius mixed release suffer airport minute',
+  // remember to change this to your own phrase!
+  'https://goerli.infura.io/v3/5187fd9b46f14d98a57d5da7fd5b06c9'
+  // remember to change this to your own endpoint!
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
